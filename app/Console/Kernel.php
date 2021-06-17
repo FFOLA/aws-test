702d22aaa5d951ec0c62cdf7cf643601dcf53149
Folakunmi Aremu <folakunmi.aremu@princepsfinance.com>
2021-06-17 11:26:10 +0100

    Cron job test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,12 +25,12 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('awaiting:disbursement')->everyFiveMinutes()->withoutOverlapping()->appendOutputTo(storage_path("logs/cron.log"));
-        $schedule->command('readyfordeduction')->everyFiveMinutes()->withoutOverlapping()->appendOutputTo(storage_path("logs/cron.log"));
-        $schedule->command('readyfor:disbursement')->everyFiveMinutes()->withoutOverlapping()->appendOutputTo(storage_path("logs/cron.log"));
-        $schedule->command('deduction:setup-awaiting')->everyFiveMinutes()->withoutOverlapping()->appendOutputTo(storage_path("logs/cron.log"));
-        $schedule->command('rsp:repayment')->everyFiveMinutes()->withoutOverlapping()->appendOutputTo(storage_path("logs/cron.log"));
-        $schedule->command('test:name')->everyMinute()->withoutOverlapping()->appendOutputTo(storage_path("logs/cron.log"));
+        // $schedule->command('awaiting:disbursement')->everyFiveMinutes()->withoutOverlapping()->appendOutputTo(storage_path("logs/cron.log"));
+        // $schedule->command('readyfordeduction')->everyFiveMinutes()->withoutOverlapping()->appendOutputTo(storage_path("logs/cron.log"));
+        // $schedule->command('readyfor:disbursement')->everyFiveMinutes()->withoutOverlapping()->appendOutputTo(storage_path("logs/cron.log"));
+        // $schedule->command('deduction:setup-awaiting')->everyFiveMinutes()->withoutOverlapping()->appendOutputTo(storage_path("logs/cron.log"));
+        // $schedule->command('rsp:repayment')->everyFiveMinutes()->withoutOverlapping()->appendOutputTo(storage_path("logs/cron.log"));
+        // $schedule->command('test:name')->everyMinute()->withoutOverlapping()->appendOutputTo(storage_path("logs/cron.log"));
 
     }
 
